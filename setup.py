7a2cbff66e4a1096dfa8e57e46e6ee6d55874014
Kirill Sumorokov <sumorokov.k@gmail.com>
2017-03-08 22:41:25 +0500

    Rewrite and simplify playbooks

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='passport',
-    version='0.1.0',
+    version='1.0.0',
     url='https://passport.clayman.pro',
     license='MIT',
     author='Kirill Sumorokov',
